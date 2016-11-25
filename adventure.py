4b4fd8c91dd4dd1ca72c4a0d75e38ff670cdd300
ben knowlson <bknowlson18@VenerableBede.schools.sunderland.gov.uk>
2016-11-25 09:55:55 +0000

    Changed quiz & adventure

--- adventure.py
+++ adventure.py
@@ -1,10 +1,272 @@
 # An adventure game
 
-def room1():
+from time import sleep
+from random import randint
 
-    ... make a game
+#BENDOG, [FRIENDLY] MAIN CHARECTAR, PREPARATION AND DECISION
+bendog_health = 100
+bendog_dmg = 4
+bendog_money = 250
+
+#NEGAN, [FRIENDLY] WEAPON DEALER
+negan_health = 100
+negan_dmg = 19
+negan_money = 500
+
+#ADBREZZ, [FRIENDLY] ARMOR DEALER
+adbrezz_health = 100
+adbrezz_dmg = 4
+adbrezz_money = 1250
+
+#TOTALLYNOTALEX, [TRAITOR] CURRENCY HOLDER (BANK)
+totallynotalex_health = 100
+totallynotalex_dmg = 4
+totallynotalex_money = 4750
+
+#BOZZMAN, [ENEMY] HACKER
+bozzman_health = 100
+bozzman_dmg = 1
+bozzman_money = 1500
+    
+#BOSS MICKY, [MAFIA LEADER] MENTAL STRENGTH, LEADER OF BOZZMAN
+bossmicky_health = 250
+bossmicky_dmg = 75
+bossmicky_money = 0
+
+#MR CHARLTON, [CODE MAKER] TEACHER
+charlton_health = 100
+charlton_dmg = 4
+charlton_money = 50
+
+#100 is the base health, armor bought from adbrezz increases health
+#4 is base damage, buying guns from Negan will increase base damage
+#Alex will keep your money safe, or will he?
+
+def pausedash():
+    sleep(2)
+    print("")
+    print("---------------------------------------------------------------------------------------------")
+    print("")
+
+def intro():
+
+#Intro to the game##################################################################################################################################################################################
+    print("4:43 PM, Base of operations...")
+    sleep(2)
+    print("Negan: We must form an alliance to take out this Micky guy. He has been abusing to many innocent people online.")
+    sleep(2)
+    print("Alex: I know, Tom Pattison told me, he's caused at least 6 people to quit twitter in the last month...")
+    sleep(2)
+    print("Negan: Exactly, we must stop him before it gets out of hand, you guys in?")
+    sleep(2)
+    print("Alex: Yeah.")
+    sleep(2)
+    print("Adam: Sure.")
+    sleep(2)
+    intro1 = input("Negan: Bendog? (Yes/No): ")
+    if intro1.lower() == "no":
+        sleep(2)
+        print("*Negan pull's out a Glock-18*")
+        sleep(2)
+        print("Negan: We must hide the evidence...")
+        sleep(2)
+        print("*Negan pulls the trigger aiming between your eyes*")
+        sleep(2)
+        print("")
+        intro2 = input("Nice try, want to try again? (Yes/No): ")
+        if intro2.lower() == "yes":
+            sleep(2)
+            print("You try again...")
+            pausedash()
+            intro()
+        elif intro2.lower() == "no":
+            sleep(2)
+            print("Fair enough...")
+            quit
+    elif intro1.lower() == "yes":
+        sleep(2)
+        print("Negan: Nice, lets get going...")
+        sleep(2)
+        print("The team setup for their operation over the next couple of days, now the real story unfolds...")
+        pausedash()
+        story1()
+
+# STORY 1 FUNCTIONS ################################################################################################################################################################################
+
+def story1_1():
+
+    global bendog_money, totallynotalex_money
+    
+    bendog_money == bendog_money - 250
+    totallynotalex_money == totallynotalex_money + 250
+    sleep(1)
+    print("Alex: Cheers, I knew you could trust me mate")
+    print("Alex: *puts money in pocket*")
+    sleep(2)
+    zzzzzz = input("Bendog: ")
+    sleep(2)
+    print("Alex: Right, I'll leave you to your thing. Seeya mate...")
+    sleep(2)
+
+def story1_2():
+
+    sleep(1)
+    print("Alex: Whatever, Negan and Adam said to meet them in the front room whenever you're ready...")
+    sleep(2)
+        
+def story1fightresult():
+
+    story1fight1()
+    story1fight2()
+    story1fight3()
+
+def story1fight1():
+
+    if x == 1:
+        print("*You outsrength Alex and push him onto the floor, reducing his health by 10 and taking your money back*")
+        totallynotalex_health = totallynotalex_health - 10
+        sleep(2)
+        print("Alex: Geez Ben, I was only trying to help")
+        sleep(2)
+        print("Bendog: Shut up man ya radgy, I'll knock ya block off")
+        sleep(2)
+        print("Negan: *Shouts* Ben, please come to the front room ASAP")
+
+def story1fight2():
+
+    if x == 2:
+        print("*Alex swings a hefty right hook and hits you in the temple where it could be fatal*")
+        bendog_money = bendog_money - 250
+        totallynotalex_money = totallynotalex_money + 250
+        sleep(2)
+        xx = randint(1, 10)
+        if xx == 10:
+            print("*It does prove fatal, how embarrasing...*")
+            sleep(2)
+            print("")
+            print("GAME OVER, and you don't get to retry because your death scene was too embarrasing, sorry :)")
+            exit
+        else:
+            print("*It wasn't fatal, but you do lose 10 health*")
+            sleep(2)
+            print("Alex: Cheers hew...")
+            sleep(2)
+            print("Bendog: Feeble ####!")
+            sleep(1)
+            print("Alex: Oh, also Negan wants to see you at the front room marra, you'd best clean up first")
+
+def story1fight3():
+
+    if x == 3:
+        print("*The bundle of money tears in half*")
+        bendog_money = bendog_money - 250
+        sleep(2)
+        print("Bendog: Well that was brilliant, wasn't it")
+        sleep(2)
+        print("Alex: Shouldn't have fought back")
+        sleep(2)
+        print("Bendog: #### off ya cocky #####...")
+        sleep(2)
+        print("Alex: Calm down, Negan says he wants you in the front room before long anyway")  
+
+def nofight():
+
+    print("*Alex takes the money and puts it in his pocket*")
+    bendog_money = bendog_money - 250
+    totallynotalex_money = totallynotalex_money + 250
+    sleep(2)
+    print("Bendog: Whyaye man...")
+    sleep(2)
+    print("Alex: Hah, taxed")
+    sleep(2)
+    print("Bendog: Better keep it safe, me nan gave is that for me birthday")
+    sleep(2)
+    print("*Alex smiles, savagely*")
+    sleep(1)
+    print("Alex: I will, Negan said to see him in the front room, catch you in a bit")
+    sleep(2)
+    print("Bendog: Aight...")
+    story2()
+
+def story1():
+
+    global bendog_health
+    global bendog_money
+    global totallynotalex_health
+    global totallynotalex_money
+
+    print("12:53 AM, Base of operations...")
+    sleep(2)
+    print("Alex: I'm now the financial organiser, want me to keep your money in the pot? I see you have 250 quid...")
+    sleep(1)
+    print("A: Aye, go on then")
+    print("B: Nor, sorry pal")
+    print("C: Can't I just keep it myself?")
+    story1a = input("A, B or C: ")
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    if story1a.lower() == "a":
+        story1_1()
+    elif story1a.lower() == "b":
+        story1_2()
+    elif story1a.lower() == "c":
+        sleep(1)
+        print("Alex: Nah, give it here.")
+        sleep(1)
+        ("*Alex tries to snatch the money*")
+        sleep(2)
+        story1ac = input("Fight back? (Yes/No): ")
+    if story1ac.lower() == "yes":
+        sleep(2)
+        x = randint(1, 3)
+    if story1ac.lower() == "yes":
+        story1fightresult()
+    elif story1ac.lower() == "no":
+        nofight()
+    story2()
+
+# STORY 2, JUST A SMALL CUTSCENE... ##############################################################################################################################################################
+
+def story2():
+
+    pausedash()
+
+    print("1:05 PM, Frontroom, Base of operations")
+    sleep(2)
+    print("Negan: Ok guys, we are ready to start with this operation")
+    sleep(2)
+    print("Adam: Nice one")
+    sleep(2)
+    print("Negan: As you all know we have sorted our roles in this operation")
+    sleep(2)
+    print("Bendog: I didn't know")
+    sleep(2)
+    print("Negan: Well, I'm going to be dealing weapons")
+    sleep(1)
+    print("Adam: I'll get protection and armor sorted")
+    sleep(1)
+    print("Alex: I'll tax yer cash")
+    sleep(1)
+    print("Bendog: And I'll take charge of this mission")
+    sleep(1)
+    print("Negan: Sound. Then lets get stuck in!")
+    
 
 
+
+def hubguide():
+# This is going to be a menu, and a guide for it
+
+    pausedash()
+
+    sleep(2)
+        
+    
+    
+    
+# This is no where near done hew :(
+    
 
 
 
@@ -14,4 +276,4 @@ def room1():
 # Leave this at the bottom - it makes room1 run automatically when you
 # run your code.
 if __name__ == "__main__":
-    room1()
+    intro()
--- quiz.py
+++ quiz.py
@@ -2,9 +2,271 @@
 
 def quiz():
 
-    ... make a quiz!
+    import time
+    
+    score = 0
+    rank = ""
 
+    print("This is the pokemon quiz!")
+    time.sleep(1)
 
+    username = input("Username: ")
+
+    print("Hello " + username + ". Welcome to Ben Knowlson's pokemon quiz.")
+    time.sleep(3)
+    print("You will have to answer 20 questions and depending on your score you'll recieve a Poke'rank! Enjoy! :)")
+    time.sleep(5)
+
+    q1 = input("How many generations of Pokemon are there?: ")
+
+    if q1 == "6":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q2 = input("Which company developed Pokemon?: ")
+
+    if q2.lower() == "nintendo":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q3 = input("What year was the first Pokemon game released?: ")
+
+    if q3 == "1996":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q4 = input("Name an original starter Pokemon: ")
+
+    if q4.lower() in "squirtlecharmanderbulbasaur":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q5 = input("What is the name of the Professor who gives you your first pokemon?: ")
+
+    if "oak" in q5.lower():
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    print("----------------------------------------------------------------------")
+    print("So far your score is", score, "out of 5! Questions are now harder!")
+    print("----------------------------------------------------------------------")
+    time.sleep(3)
+
+    q6 = input("Who is Raichu an evolution of?: ")
+
+    if q6.lower() == "pikachu":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q7 = input("What is the original name of the young pokemon trainer you control throughout the Pokemon series?: ")
+
+    if q7.lower() == "ash":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q8 = input("What generation is the legendary Pokemon 'Entei' from?: ")
+
+    if q8 == "2":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q9 = input("Which lake is the Shiny Gyrados found in Pokemon Soul Silver?: ")
+
+    if q9.lower() in "lake of rage":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q10 = input("How many evolutions of Eevee are their between Gen 1-2?: ")
+
+    if q10 == "5":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+    
+    print("----------------------------------------------------------------------")
+    print("So far your score is", score, "out of 10! Questions are now EVEN harder!")
+    print("----------------------------------------------------------------------")
+    time.sleep(3)
+
+    q11 = input("Which ball is more powerful: Master ball or Ultra ball?: ")
+
+    if "master" in q11.lower():
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q12 = input("What generation was the region of Sinnoh introduced?: ")
+
+    if q12 == "4":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q13 = input("Where is Kanto in direcion North/East/South/West from Sinnow?: ")
+
+    if "south" in q13.lower():
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q14 = input("Who are the original villains in the Kanto based Pokemon games/series'?: ")
+
+    if q14.lower() == "team rocket":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q15 = input("What is the name of the only 1st gen Pokemon beginning with the letter I? (Hint: Starter pokemon evolution): ")
+
+    if q15.lower() == "ivysaur":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    print("----------------------------------------------------------------------")
+    if score > 10:
+        print("Wow! Your score is", score, "out of 15! These questions should catch you off guard!")
+    else:
+        print(score, "score? Keep going", username, "and good luck with the following questions...")
+    print("----------------------------------------------------------------------")
+    time.sleep(3)
+
+    print("The following questions are almost impossible unless you played Pokemon as much as myself...")
+    time.sleep(2)
+
+    q16 = input("Where is the glitched Pokemon mssingno found in Pokemon Red/Blue?: ")
+
+    if "cinnibar island" in q16.lower():
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q17 = input("How many legendary pokemon are there in Gen 4?: ")
+
+    if q17 == "14":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q18 = input("Who is the first pokemon to talk in the Anime TV series?: ")
+
+    if q18.lower() == "meowth":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    q19 = input("Which Pokemon was the first to ever be created?: ")
+
+    if q19.lower() == "rhydon":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+
+    print("Now, the hardest question... You are", 20 - score, "score away from the legendary 20/20!")
+
+    q20 = input("Which Pokemon is Gengar believed to be the shadow of?: ")
+
+    if q20.lower() == "clefable":
+        print("Correct")
+        score = score + 1
+
+    else:
+        print("Incorrect")
+    time.sleep(1)
+# SPARE SPACE TO CONTINUE QUIZ
+#
+#
+
+    if score == 0:
+        print("Wow,", score, "score? You're bad at Pokemon")
+
+    if 0 < score <= 4:
+        rank = "casual"
+
+    elif 4 < score <= 8:
+        rank = "trainer"
+
+    elif 8 < score <= 12:
+        rank = "expert"
+
+    elif 12 < score <= 16:
+        rank = "mythical"
+
+    elif 16 < score <= 19:
+        rank = "legend"
+
+    elif score == 20:
+        rank = "mewtwo"
+
+    print("I have ranked you", rank, "based on your score of", score, "out of 20! GG", username)
+
+    
 
 
 
@@ -12,3 +274,4 @@ def quiz():
 # run your code.
 if __name__ == "__main__":
     quiz()
+
